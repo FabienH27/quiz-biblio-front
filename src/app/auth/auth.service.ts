2605414878729e482dac65dc7deb8341319ae93c
Fabien H <fhannon60@gmail.com>
2024-11-25 16:20:55 +0100

    improve log in process & security

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,51 +1,74 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { tap } from 'rxjs/operators';
-import { BehaviorSubject } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { jwtDecode } from 'jwt-decode';
 import { AlertService } from '../services/alert.service';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  port = 32770;
+  port = 32768;
   httpClient = inject(HttpClient);
   baseUrl = `https://localhost:${this.port}/api`; //TODO : move to config
 
-  private userTokenSource = new BehaviorSubject<string | null>(localStorage.getItem('authUser'));
-  userToken$ = this.userTokenSource.asObservable();
-
   alertService = inject(AlertService);
+  router = inject(Router);
+
+  private userInfoSubject = new BehaviorSubject<{ userName: string; userId: string } | null>(
+    this.getUserInfoFromLocalStorage()
+  );
+  userInfo$ = this.userInfoSubject.asObservable();
+
+  localStorageKey = 'userInfo';
 
   register(data: any) {
-    return this.httpClient.post(`${this.baseUrl}/Auth/register`, data);
+    return this.httpClient.post(`${this.baseUrl}/auth/register`, data);
   }
 
   login(data: any) {
-    return this.httpClient.post(`${this.baseUrl}/Auth/login`, data)
-      .pipe(tap((result) => {
-        const token = (result as any).token;
-        localStorage.setItem('authUser', token);
-        this.userTokenSource.next(token);
+    return this.httpClient.post(`${this.baseUrl}/auth/login`, data, { withCredentials: true})
+      .pipe(tap(() => {
         this.alertService.showAlert("Successfully logged in!");
+        this.router.navigate(['/']);
       }));
   }
 
   logout() {
-    localStorage.removeItem('authUser');
+    this.httpClient.post(`${this.baseUrl}/auth/logout`, {}, { withCredentials: true}).subscribe({
+      next: () => {
+        this.userInfoSubject.next(null);
+        localStorage.removeItem(this.localStorageKey);
+        this.alertService.showAlert("Successfully logged out!");
+      },
+      error: err => console.error(err)
+    });
+  }  
+
+  getUserInfo(): Observable<{userName: string, userId: string}> {
+    const url = `${this.baseUrl}/auth/user-info`;
+    return this.httpClient.get<{userName: string, userId: string}>(url, {withCredentials: true });
   }
 
-  isLoggedIn() {
-    return localStorage.getItem('authUser') !== null;
+  setUserInfo(userInfo: { userName: string; userId: string }): void {
+    this.userInfoSubject.next(userInfo);
+    localStorage.setItem(this.localStorageKey, JSON.stringify(userInfo));
   }
 
-  getUsername(){
-    if(this.userTokenSource.value != null){
-      const tokenDecoded = jwtDecode(this.userTokenSource.value);
-      return (tokenDecoded as any).userName;
-    }
+  private getUserInfoFromLocalStorage(): { userName: string; userId: string } | null {
+    const userInfo = localStorage.getItem(this.localStorageKey);
+    return userInfo ? JSON.parse(userInfo) : null;
+  }
+
+  loginAndFetchUserInfo(credentials: any): Observable<{userName: string, userId: string}> {
+    return this.login(credentials).pipe(
+      switchMap(() => this.getUserInfo()),
+      tap(userInfo => this.setUserInfo(userInfo))
+    );
   }
 }
