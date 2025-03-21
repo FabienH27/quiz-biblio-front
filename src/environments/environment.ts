6ff8c026949d25269dd0c286756be06b50b0ff1a
Fabien H <fhannon60@gmail.com>
2025-03-21 10:52:34 +0100

    add api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,6 @@
-const port = 44360;
-
 export const environment = {
     production: true,
-    apiUrl: `https://localhost:${port}/api`,
+    apiUrl: `https://quiz-biblio-api-215101547753.europe-west9.run.app/api/`,
     bucketName: 'quiz-biblio-bucket',
     bucketUrl: 'https://storage.googleapis.com/quiz-biblio-bucket/',
 };
