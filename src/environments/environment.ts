0a3be7a3fff9df6757a666d34423cd99b139e60b
Fabien H <fhannon60@gmail.com>
2025-03-21 10:58:42 +0100

    fix api path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: `https://quiz-biblio-api-215101547753.europe-west9.run.app/api/`,
+    apiUrl: `https://quiz-biblio-api-215101547753.europe-west9.run.app/api`,
     bucketName: 'quiz-biblio-bucket',
     bucketUrl: 'https://storage.googleapis.com/quiz-biblio-bucket/',
 };
