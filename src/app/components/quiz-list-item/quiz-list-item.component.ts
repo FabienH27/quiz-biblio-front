51720e9ad15d3d2763367043e8510c820b83deba
Fabien H <fhannon60@gmail.com>
2025-04-05 14:16:15 +0200

    add quiz creator info on quiz item

--- src/app/components/quiz-list-item/quiz-list-item.component.ts
+++ src/app/components/quiz-list-item/quiz-list-item.component.ts
@@ -6,10 +6,11 @@ import { heroPlayCircle, heroQueueList, heroTrash } from '@ng-icons/heroicons/ou
 import { Observable } from 'rxjs';
 import { ImageService } from '../../services/image.service';
 import { QuizInfo } from '../../types/quiz-info';
+import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
     selector: 'app-quiz-list-item',
-    imports: [NgIconComponent, I18nPluralPipe, RouterLink, NgClass, AsyncPipe],
+    imports: [NgIconComponent, I18nPluralPipe, RouterLink, NgClass, AsyncPipe, TranslocoPipe],
     providers: [provideIcons({ heroQueueList, heroTrash, heroPlayCircle })],
     templateUrl: './quiz-list-item.component.html',
     styleUrl: './quiz-list-item.component.css'
@@ -37,7 +38,7 @@ export class QuizListItemComponent implements OnInit {
   }
 
   get themes(){
-    return this.quizData.themes.sort().join(", ").substring(0, 20) + "...";
+    return this.quizData.themes.sort().join(", ").substring(0, 15) + "...";
   }
 
   playQuiz(event: Event){
