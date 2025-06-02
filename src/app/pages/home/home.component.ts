ca9c3e1a382c66d73e8e40f397187a267d152527
Fabien Hannon <33556690+FabienH27@users.noreply.github.com>
2025-06-02 16:16:03 +0200

    Revert "Update packages (#96)" (#97)
    This reverts commit c3a7f8738aa708b3280b448ebf2e49810e52b5bb.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit } from '@angular/core';
 import { QuizService } from '../../services/quiz.service';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgIf } from '@angular/common';
 import { QuizListItemComponent } from "../../components/quiz-list-item/quiz-list-item.component";
 import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
     selector: 'app-home',
-    imports: [AsyncPipe, QuizListItemComponent, TranslocoPipe],
+    imports: [AsyncPipe, QuizListItemComponent, TranslocoPipe, NgIf],
     providers: [],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
