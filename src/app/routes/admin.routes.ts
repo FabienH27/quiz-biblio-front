d5aef0376394154da6a9a618a6ad1363d37d0de4
Fabien H <fhannon60@gmail.com>
2025-07-02 14:29:38 +0200

    set up user list page

--- src/app/routes/admin.routes.ts
+++ src/app/routes/admin.routes.ts
@@ -1,20 +1,19 @@
 import { Routes } from "@angular/router";
 import { adminGuard } from "../guards/admin.guard";
 import { AdminComponent } from "../components/layouts/admin/admin.component";
-import { AdminPanelComponent } from "../pages/admin/admin-panel/admin-panel.component";
 import { UsersListComponent } from "../pages/admin/users-list/users-list.component";
 import { ThemesListComponent } from "../pages/admin/themes-list/themes-list.component";
 
 export const adminRoutes: Routes = [
     {
         path: 'admin',
+        redirectTo: 'admin/users'
+    },
+    {
+        path: 'admin',
         component: AdminComponent,
         canActivate: [adminGuard],
         children: [
-            {
-                path: '',
-                component: AdminPanelComponent
-            },
             {
                 path: 'users',
                 component: UsersListComponent
